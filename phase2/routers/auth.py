2ea7d527f183c0237684c1923ad377d3dd92d613
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2026-07-30 14:02:39 +0500

    Integrated Auth_services

--- phase2/routers/auth.py
+++ phase2/routers/auth.py
@@ -1,61 +1,20 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-import hashlib
-import jwt
-from datetime import datetime, timedelta, timezone
 
 from phase2.database.json_store import db
 from phase2.schemas.user_schema import UserCreate, UserResponse, UserLogin, Token
+from phase3.services.auth_service import get_current_user_id, hash_password, create_access_token
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
-
-# --- 1. JWT CONFIGURATION ---
-SECRET_KEY = "b7f9a8d4e2c145f6a9c8b7d6e5f4a3b2c1d0e9f8a7b6c5d4e3f2a1b0c9d8e7f6"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 120 
-
 security = HTTPBearer(auto_error=False)
 
-# --- 2. CRYPTOGRAPHY & TOKEN GENERATION ---
-def hash_password(password: str) -> str:
-    return hashlib.sha256(password.encode()).hexdigest()
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-# --- 3. THE GATEKEEPER DEPENDENCY ---
+# --- GET USER DICT ---
 def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)) -> dict:
     if credentials is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
     token = credentials.credentials
     
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id_str = payload.get("sub")
-        
-        if user_id_str is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid authentication credentials",
-            )
-
-        # Cast the user_id back to INT for JSON_DB lookup
-        user_id = int(user_id_str)
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token has expired. Please log in again.",
-        )
-    except jwt.InvalidTokenError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-        )
+    user_id = get_current_user_id(token= token)
         
     user = db.get_user_by_id(user_id)
     if user is None:
